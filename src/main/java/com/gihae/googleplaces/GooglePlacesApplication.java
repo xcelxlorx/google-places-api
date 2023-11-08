05d539a5aef06fbe3490a242c941a108d3bbb32a
xcelxlorx <xcelxlorx@gmail.com>
2023-11-08 21:49:35 +0900

    init: Init project

---
+++ src/main/java/com/gihae/googleplaces/GooglePlacesApplication.java
@@ -0,0 +1,13 @@
+package com.gihae.googleplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePlacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePlacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gihae/googleplaces/GooglePlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihae.googleplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
